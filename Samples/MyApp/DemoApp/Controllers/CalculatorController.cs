cbff832a6cf56b675a6ab1bdaa0cd12d439de601
ahm7dkhalifa@gmail.com <ahm7dkhalifa@gmail.com>
2017-10-27 18:20:52 +0200

    Create Proxy Class

---
+++ Samples/MyApp/DemoApp/Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CalculateFromOtherApp()
+        {
+            CalculateReference.CalculateSoapClient CalculateWebServiceObj = new CalculateReference.CalculateSoapClient();
+            //string Result = CalculateWebServiceObj.Add();error
+            int AddResult = CalculateWebServiceObj.Add();
+            int AddXMLResult = CalculateWebServiceObj.AddXML();
+            //int AddXMLWithParametersResult = CalculateWebServiceObj.AddXMLWithParameters();error
+            int AddXMLWithParametersResult = CalculateWebServiceObj.AddXMLWithParameters(123,123);
+
+            return View();
+        }
+    }
+}
